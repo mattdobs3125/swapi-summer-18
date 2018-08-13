5046126e311627d569d402b5c7724cba6ee221ff
matt <matthew.dobrowolski5@gmail.com>
2018-08-13 17:27:19 -0600

    fixes

--- app/components/swapi-controller.js
+++ app/components/swapi-controller.js
@@ -10,15 +10,16 @@ function draw(data) {
   <button onclick="app.controllers.swapi.getStarships()">
   Get Starships
   </button>
+  <button onclick="app.controllers.swapi.getPeople()"> Get People </button>
   <div id="starships"></div>`
 }
 
 function drawStarships(data) {
   let starshipsElem = document.getElementById('starships')
   let template = ''
-  data.results.forEach(starship => {
+  Object.keys(data).map(k => data[k]).forEach(starship => {
     template += `<div>
-    ${starship.name}
+    ${starship.name} Pilots: ${starship.pilots.length}
     </div>`
   })
 
@@ -39,8 +40,97 @@ export default class SwapiController {
 
   getStarships() {
     console.log("HELLO FROM CONTROLLER")
-    swapiService.getStarships("{}", drawError)
+    swapiService.getStarships(drawStarships, drawError)
   }
 
-
+  getPeople() {
+    //swapiService.getPeople(console.log, drawError)
+    console.log(swapiService.people)
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import SwapiService from "./swapi-service.js";
+
+// const swapiService = new SwapiService
+// let app = document.getElementById('app')
+
+// function draw(data) {
+//   console.log(data)
+//   app.innerHTML = `
+//   <div id="error"></div>
+//   <button onclick="app.controllers.swapi.getStarships()">
+//   Get Starships
+//   </button>
+//   <div id="starships"></div>`
+// }
+
+// function drawStarships(data) {
+//   let starshipsElem = document.getElementById('starships')
+//   let template = ''
+//   data.results.forEach(starship => {
+//     template += `<div>
+//     ${starship.name}
+//     </div>`
+//   })
+
+//   starshipsElem.innerHTML = template
+
+// }
+
+// function drawError(error) {
+//   console.log(error)
+//   document.getElementById('error').innerHTML = error.message
+// }
+
+
+// export default class SwapiController {
+//   constructor() {
+//     draw()
+//   }
+
+//   getStarships() {
+//     console.log("HELLO FROM CONTROLLER")
+//     swapiService.getStarships("{}", drawError)
+//   }
+
+
+// }
